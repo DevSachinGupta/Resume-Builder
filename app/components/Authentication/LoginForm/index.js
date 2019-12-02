be7ce7c97824f0e40f5398b4b75052fe37fba38d
jitendrappr990 <jitendrappr990@gmail.com>
2019-12-02 20:58:24 +0530

    adding carousel

--- app/components/Authentication/LoginForm/index.js
+++ app/components/Authentication/LoginForm/index.js
@@ -156,18 +156,18 @@ class LoginForm extends React.Component {
             </div>
           </div>
           <div className="w-full md:w-2/3 flex flex-col flex-grow flex-shrink">
-			<div className="carousel-inner relative overflow-hidden w-full">
+			<div className="carousel-inner relative overflow-hidden h-full w-full">
 			
 
 				<input className="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden="" checked="checked"></input>
-				<div class="carousel-item absolute opacity-0">
+				<div class=" h-full w-full carousel-item absolute opacity-0" >
 					<div class="block h-full w-full bg-indigo-500 text-white text-5xl text-center">Slide 1</div>
 				</div>
 				<label for="carousel-3" class="prev control-1 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
 				<label for="carousel-2" class="next control-1 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
 				
 				<input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden=""></input>
-				<div class="carousel-item absolute opacity-0" >
+				<div class="h-full w-full carousel-item absolute opacity-0" >
 					<div class="block h-full w-full bg-orange-500 text-white text-5xl text-center">Slide 2</div>
 				</div>
 				<label for="carousel-1" class="prev control-2 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
@@ -175,7 +175,7 @@ class LoginForm extends React.Component {
 
 
 				<input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden=""></input>
-				<div class="carousel-item absolute opacity-0" >
+				<div class="h-full w-full carousel-item absolute opacity-0" >
 					<div class="block h-full w-full bg-green-500 text-white text-5xl text-center">Slide 3</div>
 				</div>
 				<label for="carousel-2" class="prev control-3 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
