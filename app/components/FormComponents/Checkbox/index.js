1364e035ef7280315c1f77ab81076dce5f8956ee
jitendrappr990 <jitendrappr990@gmail.com>
2020-01-05 19:50:48 +0530

    update forms-ui employments

--- app/components/FormComponents/Checkbox/index.js
+++ app/components/FormComponents/Checkbox/index.js
@@ -1,24 +1,50 @@
-/**
- *
- * Checkbox
- *
- */
-
 import React, { memo } from 'react';
+import cx from 'classnames';
+import { useField } from 'formik';
+import { MdCancel } from 'react-icons/md';
 import PropTypes from 'prop-types';
-// import styled from 'styled-components';
-import Text from "../Text";
+import './style.scss';
 
 function Checkbox(props) {
+  const [field, meta, helpers] = useField(props.name);
   return (
-    <div>
-      <Text type="checkbox" {...props} /> {props.text}
+    <div className={cx('inputWrapper')}>
+      <div className="label">{props.label}</div>
+      <div
+        className={cx({
+          fullWidth: props.fullWidth,
+          error: props.error,
+        })}
+      >
+        {props.inputIcon && (
+          <span className="inputIcon">{props.inputIcon}</span>
+        )}
+        <input {...field} {...props} onChange={props.onChange} />
+        {props.clearable && props.value.length > 0 && (
+          <span className="input-right-Icon cursor-pointer">
+            {<MdCancel />}
+          </span>
+        )}
+      </div>
+      {
+        meta.error && meta.touched && <div className={cx('hint', { error_hint: meta.error && meta.touched })}>
+          {props.error && props.error}
+        </div>
+      }
     </div>
   );
 }
-
+Checkbox.defaultProps = {
+  value: ""
+}
 Checkbox.propTypes = {
-  text: PropTypes.string.isRequired,
+  clearable: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+  fullWidth: PropTypes.bool.isRequired,
+  inputIcon: PropTypes.node.isRequired,
+  value: PropTypes.oneOf([PropTypes.string, PropTypes.number]),
+  error: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string,
 };
-
 export default memo(Checkbox);
