7ba0b1789cab5a3b6216b6be9536e37fbeaa1cf7
Sachin Gupta <sachingupta01@live.com>
2020-01-22 22:07:00 +0530

    Adding new component

---
+++ app/components/FormComponents/MultiselectAutocomplete/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * MultiselectAutocomplete
+ *
+ */
+
+import React, { memo } from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function MultiselectAutocomplete() {
+  return <div />;
+}
+
+MultiselectAutocomplete.propTypes = {};
+
+export default memo(MultiselectAutocomplete);
