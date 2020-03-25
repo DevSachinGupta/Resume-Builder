107b11f87d853b68706973b5fc6bbfbb2e815eb7
asif ansari <ashifa454@gmail.com>
2020-03-25 23:00:20 +0530

    Skills Form

--- app/components/MyContentForms/SkillsForms/index.js
+++ app/components/MyContentForms/SkillsForms/index.js
@@ -3,22 +3,24 @@ import { Formik, Form } from 'formik';
 import cx from 'classnames';
 import Button from '../../Button';
 import Dropdown from '../../FormComponents/Dropdown';
+import Tag from '../../Tag';
 import './style.scss';
 
 function SkillsForm() {
   const skillData = [
     { key: 'Music', value: 'Music' },
     { key: 'B', value: 'B' },
-    { key: 'C', value: 'C' },
+    { key: 'Delhi', value: 'Delhi' },
+    { key: 'Kolkata', value: 'Kolkata' },
+    { key: 'Mumbai', value: 'Mumbai' },
+    { key: 'New Delhi', value: 'New Delhi' },
+    { key: 'Ghaziabad', value: 'Ghaziabad' },
+    { key: 'Other', value: 'Other' },
   ];
   const blankSkillsField = [];
-  const [data, setData] = useState({
-    datValues: [],
-  });
+  const [data, setData] = useState([]);
   const updateData = value => {
-    setData({
-      datValues: [...data.datValues, value],
-    });
+    setData([...data, value]);
   };
   return (
     <Formik
@@ -29,19 +31,26 @@ function SkillsForm() {
       }}
     >
       {({ handleSubmit, isSubmitting }) => (
-        <Form className="socialFormContainer">
-          <Dropdown
-            onSelect={value => console.log(value)}
-            name="skils"
-            multiSelect
-            options={skillData}
-          />
-          <div className={cx('footerContainer')}>
-            <Button as="submit" fullWidth type="primary">
-              Save Details
-            </Button>
+        <React.Fragment>
+          <div className="tagContainer">
+            {data.map(item => (
+              <Tag>{item.value}</Tag>
+            ))}
           </div>
-        </Form>
+          <Form className="socialFormContainer">
+            <Dropdown
+              onSelect={updateData}
+              name="skils"
+              multiSelect
+              options={skillData}
+            />
+            <div className={cx('footerContainer')}>
+              <Button as="submit" fullWidth type="primary">
+                Save Details
+              </Button>
+            </div>
+          </Form>
+        </React.Fragment>
       )}
     </Formik>
   );
