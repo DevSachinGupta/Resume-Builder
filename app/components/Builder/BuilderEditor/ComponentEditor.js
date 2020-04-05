fd16008054183074afbd42ef642cd8ff019de6a5
jitendrappr990 <jitendrappr990@gmail.com>
2020-04-05 17:35:38 +0530

    grapesjs testing

--- app/components/Builder/BuilderEditor/ComponentEditor.js
+++ app/components/Builder/BuilderEditor/ComponentEditor.js
@@ -29,23 +29,38 @@ async function getJsonFromData(sectionId, payload, editor) {
   const component = editor.DomComponents.componentsById[id];
   const newComponent = JSON.stringify(component);
   const parentComponent = editor.DomComponents.componentsById[id].parent();
-  const pcomponent = editor.DomComponents.componentsById[id];
+
+  console.log((JSON.stringify(parentComponent)));
+  const pcomponent = JSON.parse(JSON.stringify(parentComponent));
   // const index = 1;
   // parentComponent.components(' ');
-  console.log('pcomponent', parentComponent.components);
-  await parentComponent.components().forEach(i => {
-    i.remove();
-  });
-  console.log('pcomponent', parentComponent);
+  // console.log('pcomponent', JSON.stringify(pcomponent.components.length,null,2));
+  let k=0;
+  // pcomponent.components = [];
+  // await pcomponent.components.forEach(i => {
+  //   console.log(k)
+  //   k=k+1;
+  //   console.log('pcomponent', JSON.stringify(i,null,2));
+  //   delete pcomponent.components[k];
+  // });
+  // parentComponent.replaceWith(pcomponent);
+  // console.log('pcomponent', JSON.stringify(parentComponent,null,2));
   // console.log("parent component : -", JSON.stringify(parentComponent, null, 2));
   // editor.DomComponents.componentsById[id].remove();
   // parentComponent.components("");
+  let tempComponent = [];
   payload.forEach((data, index) => {
     const tempComp = JSON.parse(newComponent);
     setLeaf(tempComp, sectionId, index, payload[index]);
     tempComp.attributes.id = `${sectionId}_${index}`;
-    parentComponent.append(tempComp);
+    console.log("t comp:", tempComp)
+    // parentComponent.append(tempComp);
+    tempComponent.push(tempComp);
   });
+  console.log("temp com: ", tempComponent)
+  pcomponent.components = tempComponent;
+  console.log("com: ", pcomponent)
+  parentComponent.replaceWith(pcomponent);
 }
 function setLeaf(objt, sectionId, index, data) {
   if (objt.components.length === 0) {
