f710e30653ec1692d20d212464e2354599707fed
Sachin Gupta <sachingupta01@live.com>
2020-05-01 16:33:25 +0530

    Some Updates....

--- app/components/Features/index.js
+++ app/components/Features/index.js
@@ -11,10 +11,46 @@ import Sidebar from './Sidebar';
 import BodyLayout from './BodyLayout';
 import Footer from './Footer';
 import './style.scss';
+import templateList from './templatesList';
 // import PropTypes from 'prop-types';
 // import styled from 'styled-components';
 
 function Features() {
+  const [filters, setFilters] = React.useState({
+		"pricing": "-1",
+		"category": [],
+		"rating": -1,
+		"pagesize": 12,
+		"sortOrder": -1, // no of user === then rating
+		"viewType": "Grid",
+  });
+  function updateFilter(key, value) {
+    const data = {};
+    data[key] = value;
+    setFilters({...filters, ...data});
+  }
+  function getFilteredList(){
+    let newList = [];
+    if(filters.pricing != "-1") {
+      const data = filters.pricing.split("-");
+      newList = templateList.filter(d => {
+        if(data.length > 1) {
+          return parseFloat(d.price) <= parseFloat(data[0]) && parseFloat(d.price) <= parseFloat(data[1]);
+        } else if (data.indexOf(">") > 0){
+          return parseFloat(d.price) >= parseFloat(data[0].replace(">", ""));
+        } else if (data.indexOf("<") > 0) {
+          return parseFloat(d.price) <= parseFloat(data[0].replace("<", ""));
+        }
+      })
+    }
+    if(filters.category.length !== 0) {
+      newList = newList.filter(d => d.category.some(s => filters.category.includes(s)));
+    }
+    if(filters.rating !== -1) {
+      newList = newList.filter(d => d.rating === filters.rating);
+    }
+  };
+  
   return (
     <div className="bg-gray-200">
       <div>
@@ -29,10 +65,10 @@ function Features() {
           </div> */}
           <div className="flex px-10">
             <div className="w-1/4 mt-2">
-              <Sidebar />
+              <Sidebar updateFilters={updateFilter}/>
             </div>
             <div className="w-3/4 ml-6 mt-2">
-              <BodyLayout />
+              <BodyLayout viewType={filters.viewType} templateItems={getFilteredList}/>
             </div>
           </div>
         </div>
